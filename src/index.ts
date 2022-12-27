2f2eee2dd0a548c9296621ba8af0608e429f5907
infojunkie <karim.ratib@gmail.com>
2022-12-27 10:32:30 -0800

    Don't throw on Web MIDI failure

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ export * from './Player';
 
 export async function midiOutputs(): Promise<Array<WebMidi.MIDIOutput>> {
   if (navigator.requestMIDIAccess) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       navigator.requestMIDIAccess().then(
         (midi) => {
           resolve([...midi.outputs.values()]);
         },
-        (err) => reject(err),
+        (err) => {
+          console.error(`Error initializing Web MIDI: ${err}`);
+          resolve([]);
+        },
       );
     });
   }
