be565c5c3570393728cd49e69bde55e8c97bdd12
infojunkie <karim.ratib@gmail.com>
2023-05-24 19:57:11 -0700

    Fix #27 Cleanup package.json

--- dist/types/VerovioConverter.d.ts
+++ dist/types/VerovioConverter.d.ts
@@ -5,15 +5,6 @@ import type { IMidiConverter, MeasureTimemap } from './IMidiConverter';
  * Implementation of IMidiConverter that uses the Verovio library to convert a MusicXML file to MIDI and timemap.
  * @see https://book.verovio.org/toolkit-reference/toolkit-methods.html#rendertomidi and
  * @see https://book.verovio.org/toolkit-reference/toolkit-methods.html#rendertotimemap
- *
- * At this time, this converter does not handle repeats and jumps, so either:
- * - Use this converter when you are sure that the MusicXML contains no repeats or jumps
- * - Pre-unroll the score, e.g. using musicxml-mma's musicxml-unroll transformation
- *   @see https://github.com/infojunkie/musicxml-mma/blob/main/musicxml-unroll.xsl
- *
- * You can also participate in the discussion on the Verovio repo
- * @see https://github.com/rism-digital/verovio/issues/235
- *
  */
 export declare class VerovioConverter implements IMidiConverter {
     private _unroll;
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import externalGlobals from "rollup-plugin-external-globals";
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import json from '@rollup/plugin-json';
 
