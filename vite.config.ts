48d0f1e871dd2f9a9da4a007dad94ab97a9e1edc
typedin <antoine@typedin.dev>
2025-10-10 20:43:56 +0200

    ignore demo folder (a symlink)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    exclude: [
+      ...configDefaults.exclude,
+      "demo/"
+    ]
   }
 })
